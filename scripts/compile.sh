0ab925b2c17dcea5cbde0e704c7bfd94d1a409f2
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2025-05-22 01:24:23 +0700

    build: automatic checksum update

---
+++ scripts/compile.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if ! test -d "./platforms"; then
+  exit 1
+fi
+
+list_checksum() {
+  for platform_path in ./platforms/*.sh; do
+    platform_name="$(basename "$platform_path")"
+    if test "$platform_name" = '*.sh'; then
+      break
+    fi
+
+    hash() {
+      "$1" "./platforms/$platform_name" | cut -d' ' -f1
+    }
+
+    printf '{'
+    printf '"filename":"%s",' "$platform_name"
+    printf '"m":"%s",' "$(hash md5sum)"
+    printf '"s1":"%s"' "$(hash sha1sum)"
+    printf '}\n'
+  done
+}
+
+checksum_json() {
+  list_checksum | jq -sc 'map({(.filename|tostring): del(.filename)}) | add'
+}
+
+sed -i '.bck' "s/CLIGPT_CHECKSUM=.*/CLIGPT_CHECKSUM='"$(checksum_json)"'/g" ./gpt
