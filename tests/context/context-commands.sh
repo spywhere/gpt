5d7348a973124c0f390dc3ca282578588411027f
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2025-06-15 05:30:34 +0700

    test: add more tests for context commands

--- tests/context/context-commands.sh
+++ tests/context/context-commands.sh
@@ -2,7 +2,22 @@
 
 describe_context_commands() {
   test_should_show_help_menu() {
-    echo "/help" | combined_gpt -c
+    printf '/help\n' | combined_gpt -c
+    expect all to match pattern
+  }
+
+  test_should_show_current_context() {
+    printf '/info\n' | combined_gpt -c
+    expect all to match pattern
+  }
+
+  test_should_undo_last_conversation() {
+    printf 'hello\nhow are you\n/dump\n/undo\n/dump\n' | combined_gpt -c
+    expect all to match pattern
+  }
+
+  test_should_clear_all_conversation() {
+    printf 'hello\nhow are you\n/dump\n/clear\n/dump\n' | combined_gpt -c
     expect all to match pattern
   }
 }
